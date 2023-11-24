133b6fb4e38166116a4e8247431cabf281cead7f
dolvin17 <huertas876@gmail.com>
2023-11-24 23:58:32 +0100

    adding some tests

--- push_swap.c
+++ push_swap.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ghuertas <ghuertas@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/08 10:24:39 by dolvin17          #+#    #+#             */
-/*   Updated: 2023/11/02 05:06:07 by ghuertas         ###   ########.fr       */
+/*   Updated: 2023/11/24 22:19:30 by dolvin17         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,13 +39,18 @@ int	main(int argc, char **argv)
 	{
 		order_five(stacks);
 	}
-	//printf("after stack_b pos cero %d\n", stacks[1].content[0]);
-	// printf("after stack_b pos uno  %d\n", stacks[1].content[1]);
-	//printf("after stack_a pos cero %d\n", stacks[0].content[0]);
-	//printf("after stack_a pos uno  %d\n", stacks[0].content[1]);
-	//printf("after stack_a pos dos  %d\n", stacks[0].content[2]);
-	//printf("after stack_a pos tres %d\n", stacks[0].content[3]);
-	//printf("after stack_a pos cuat %d\n", stacks[0].content[4]);
+	/*
+	printf("after stack B pos cero %d\n", stacks[1].content[0]);
+	printf("after stack B pos uno  %d\n", stacks[1].content[1]);
+	printf("after stack B pos dos  %d\n", stacks[1].content[2]);
+	printf("after stack B pos tres %d\n", stacks[1].content[3]);
+	printf("after stack B pos cuat %d\n", stacks[1].content[4]);
+	printf("after stack_a pos cero %d\n", stacks[0].content[0]);
+	printf("after stack_a pos uno  %d\n", stacks[0].content[1]);
+	printf("after stack_a pos dos  %d\n", stacks[0].content[2]);
+	printf("after stack_a pos tres %d\n", stacks[0].content[3]);
+	printf("after stack_a pos cuat %d\n", stacks[0].content[4]);
+	*/
 	free(stacks[0].content);
 	free(stacks[1].content);
 	return (0);
