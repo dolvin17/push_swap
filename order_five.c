be18358356e8769146d599dda8d2c9fcd1320830
dolvin17 <huertas876@gmail.com>
2023-11-24 23:56:15 +0100

    working with 10 movs or les

--- order_five.c
+++ order_five.c
@@ -3,83 +3,61 @@
 /*                                                        :::      ::::::::   */
 /*   order_five.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ghuertas <ghuertas@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 23:48:57 by ghuertas          #+#    #+#             */
-/*   Updated: 2023/11/02 05:56:28 by ghuertas         ###   ########.fr       */
+/*   Updated: 2023/11/24 23:46:34 by dolvin17         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	find_max_index(int	*value, int size)
+t_stack	place_min_on_top(t_stack *stack_a)
 {
-	int	max_index;
-	int	max_value;
+	int	min_value;
 	int	index;
+	int	min_index;
 
-	if (size <= 0)
-		return (EXIT_FAILURE);
-	max_index = 0;
-	max_value = value[0];
-	index = 1;
-	while (index < size)
+	min_value = INT_MAX;
+	min_index = -1;
+	index = 0;
+	while (index < stack_a->size)
 	{
-		if (value[index] > max_value)
+		if (stack_a->content[index] < min_value)
 		{
-			max_index = index;
-			max_value = value[index];
+			min_value = stack_a->content[index];
+			min_index = index;
 		}
 		index++;
 	}
-	return (max_index);
-}
-
-void	just_order_first(t_stack stacks[2])
-{
-	int		*value;
-
-	value = stacks[0].content;
-	if (stacks[0].size < 6)
+	if (min_index != -1 && min_index != 0)
 	{
-		if (value[2] > value[0] && value[0] > value[1])
-			ft_sa("sa\n", stacks);
-		if (value[0] > value[2] && value[2] > value[1])
-			ft_ra("ra\n", stacks);
-		if (value[1] > value[0] && value[0] > value[2])
-			ft_rra("rra\n", stacks);
-		if (value[0] > value[1] && value[1] > value[2])
+		if (min_index <= stack_a->size / 2)
 		{
-			ft_ra("ra\n", stacks);
-			ft_sa("sa\n", stacks);
+			while (min_index-- > 0)
+				ft_ra("ra\n", stack_a);
 		}
-		if (value[1] > value[2] && value[2] > value[0])
-			ft_sa("sa\n", stacks);
+		else
+		{
+			min_index = stack_a->size - min_index;
+			while (min_index-- > 0)
+				ft_rra("rra\n", stack_a);
+		}
 	}
+	return (*stack_a);
 }
 
-int	order_five(t_stack stacks[2])
+t_stack	order_five(t_stack stacks[2])
 {
-	int		*value;
-	int		max_index;
-
+	stacks[0] = place_min_on_top(&stacks[0]);
 	if (is_sorted(stacks[0]))
-		return (EXIT_SUCCESS);
-	value = stacks[0].content;
-	max_index = find_max_index(value, stacks[0].size);
-	if (stacks[0].size < 6)
-	{
-		if (max_index == 0 || max_index == 1 || max_index == 2)
-			ft_ra("ra\n", stacks);
-		if (max_index == 0 || max_index == 1 || max_index == 2)
-			ft_ra("ra\n", stacks);
-		just_order_first(stacks);
-		ft_push_b("pb\n", &stacks[0], &stacks[1]);
-		ft_push_b("pb\n", &stacks[0], &stacks[1]);
-		order_three(stacks);
-		ft_push_a("pa\n", &stacks[0], &stacks[1]);
-		ft_push_a("pa\n", &stacks[0], &stacks[1]);
-	
-	}
-	return (order_five(stacks));
+		return (stacks[0]);
+	ft_push_b("pb\n", &stacks[0], &stacks[1]);
+	stacks[0] = place_min_on_top(&stacks[0]);
+	ft_push_b("pb\n", &stacks[0], &stacks[1]);
+	if (!is_sorted(stacks[0]))
+		stacks[0] = order_three(&stacks[0]);
+	ft_push_a("pa\n", &stacks[0], &stacks[1]);
+	ft_push_a("pa\n", &stacks[0], &stacks[1]);
+	return (stacks[0]);
 }
