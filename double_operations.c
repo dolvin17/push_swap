544b19c62c54a4d1994c1fe4c386863db82fe2af
dolvin17 <huertas876@gmail.com>
2023-12-12 04:56:15 +0100

    implement rr&&rrr double-stack operations

---
+++ double_operations.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   double_operations.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/14 22:21:50 by dolvin17          #+#    #+#             */
+/*   Updated: 2023/12/09 14:15:42 by dolvin17         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static void	rotate(t_stack **head)
+{
+	t_stack	*last_node;
+	t_stack	*aux;
+
+	if (!*head || !(*head)->next)
+		;
+	aux = *head;
+	while (aux->next)
+		aux = aux->next;
+	last_node = aux;
+	last_node->next = *head;
+	*head = (*head)->next;
+	(*head)->prev = NULL;
+	last_node->next->prev = last_node;
+	last_node->next->next = NULL;
+}
+
+static	void	reverse_rotate(t_stack **head)
+{
+	t_stack	*last_node;
+	t_stack	*aux;
+
+	if (!*head || !(*head)->next)
+		;
+	aux = *head;
+	while (aux->next)
+		aux = aux->next;
+	last_node = aux;
+	last_node->prev->next = NULL;
+	last_node->next = *head;
+	last_node->prev = NULL;
+	*head = last_node;
+	last_node->next->prev = last_node;
+}
+
+void	ft_rr(char	*operation, t_stack **stack_a, t_stack **stack_b)
+{
+	write(1, operation, 3);
+	rotate(stack_a);
+	rotate(stack_b);
+}
+
+void	ft_rrr(char	*operation, t_stack **stack_a, t_stack **stack_b)
+{
+	write(1, operation, 4);
+	reverse_rotate(stack_a);
+	reverse_rotate(stack_b);
+}
