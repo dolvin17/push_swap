7d5c99bac880b75bdcd5b86fdb62e67bfa00a7af
dolvin17 <huertas876@gmail.com>
2023-12-15 20:29:48 +0100

    fixing returns, was causing segfaults

--- double_operations.c
+++ double_operations.c
@@ -6,7 +6,7 @@
 /*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/14 22:21:50 by dolvin17          #+#    #+#             */
-/*   Updated: 2023/12/09 14:15:42 by dolvin17         ###   ########.fr       */
+/*   Updated: 2023/12/14 22:00:57 by dolvin17         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ static void	rotate(t_stack **head)
 	t_stack	*aux;
 
 	if (!*head || !(*head)->next)
-		;
+		return ;
 	aux = *head;
 	while (aux->next)
 		aux = aux->next;
@@ -36,7 +36,7 @@ static	void	reverse_rotate(t_stack **head)
 	t_stack	*aux;
 
 	if (!*head || !(*head)->next)
-		;
+		return ;
 	aux = *head;
 	while (aux->next)
 		aux = aux->next;
