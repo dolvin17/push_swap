9d06a8da37edf05612283584376444e6d066247d
dolvin17 <huertas876@gmail.com>
2023-12-16 19:46:08 +0100

    fixing returns, overflows, segfaults, all instructions parsed

--- checker_bonus/checker.c
+++ checker_bonus/checker.c
@@ -6,11 +6,12 @@
 /*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 22:53:53 by dolvin17          #+#    #+#             */
-/*   Updated: 2023/12/13 17:48:28 by dolvin17         ###   ########.fr       */
+/*   Updated: 2023/12/16 19:25:44 by dolvin17         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "checker_bonus.h"
+
 
 void	adding_node(t_stack **stack, long nbr)
 {
@@ -19,10 +20,10 @@ void	adding_node(t_stack **stack, long nbr)
 	t_stack	*last_node;
 
 	if (!*stack)
-		;
+		return ;
 	node = malloc(sizeof(t_stack));
 	if (!node)
-		;
+		return ;
 	node->next = NULL;
 	node->data = nbr;
 	if (*stack == NULL)
@@ -43,8 +44,8 @@ void	adding_node(t_stack **stack, long nbr)
 
 void	parsing_args(t_stack **stack_a, char **argv)
 {
-	long	nbr;
-	int		i;
+	int	nbr;
+	int	i;
 
 	i = 0;
 	while (argv[i])
@@ -61,28 +62,41 @@ void	parsing_args(t_stack **stack_a, char **argv)
 	}
 }
 
-char	*match_and_move(t_stack **stack_a, t_stack **stack_b, char *line)
+void	match_and_move2(t_stack **stack_a, t_stack **stack_b, char *l)
 {
-	if (line[0] == 's' && line[1] == 'a' && line[2] == '\n')
-		sa(stack_a);
-	if (line[0] == 'r' && line[1] == 'r' && line[2] == '\n')
-		rr_both(stack_a, stack_b);
-	if (line[0] == 'r' && line[1] == 'r' && line[2] == 'r' && line[3] == '\n')
-		rrr_both(stack_a, stack_b);
-	if (line[0] == 'r' && line[1] == 'a' && line[2] == '\n')
-		ra(stack_a);
-	if (line[0] == 'r' && line[1] == 'r' && line[2] == 'a' && line[3] == '\n')
+	if (l[0] == 'r' && l[1] == 'r' && l[2] == 'a' && l[3] == '\n')
 		rra(stack_a);
-	if (line[0] == 'p' && line[1] == 'b' && line[2] == '\n')
-		push(stack_b, stack_a);
-	if (line[0] == 'p' && line[1] == 'a' && line[2] == '\n')
-		push(stack_a, stack_b);
-	if (line[0] == 'r' && line[1] == 'r' && line[2] == 'b' && line[3] == '\n')
+	else if (l[0] == 'r' && l[1] == 'r' && l[2] == 'b' && l[3] == '\n')
 		rra(stack_b);
-	if (line[0] == 'r' && line[1] == 'b' && line[2] == '\n')
+	else if (l[0] == 'r' && l[1] == 'r' && l[2] == 'r' && l[3] == '\n')
+		rrr_both(stack_a, stack_b);
+}
+
+char	*match_and_move(t_stack **stack_a, t_stack **stack_b, char *l)
+{
+	if (l[0] == 's' && l[1] == 'a' && l[2] == '\n')
+		sa(stack_a);
+	else if (l[0] == 's' && l[1] == 'b' && l[2] == '\n')
+		;
+	else if (l[0] == 's' && l[1] == 's' && l[2] == '\n')
+		;
+	else if (l[0] == 'p' && l[1] == 'a' && l[2] == '\n')
+		push(stack_a, stack_b);
+	else if (l[0] == 'p' && l[1] == 'b' && l[2] == '\n')
+		push(stack_b, stack_a);
+	else if (l[0] == 'r' && l[1] == 'a' && l[2] == '\n')
+		ra(stack_a);
+	else if (l[0] == 'r' && l[1] == 'b' && l[2] == '\n')
 		ra(stack_b);
+	else if (l[0] == 'r' && l[1] == 'r' && l[3] == '\n')
+		match_and_move2(stack_a, stack_b, l);
+	else if (l[0] == 'r' && l[1] == 'r' && l[2] == '\n')
+		rr_both(stack_a, stack_b);
 	else
-		write(1, "Error\n", 6);
+	{
+		write(1, "Error\n", 7);
+		exit(EXIT_FAILURE);
+	}
 	return (get_next_line(0));
 }
 
@@ -100,8 +114,9 @@ void	lets_check(t_stack **stack_a, t_stack **stack_b, char *line)
 		write(1, "KO\n", 3);
 	else if (!sorted(*stack_a))
 		write(1, "KO\n", 3);
-	else
+	else if (sorted(*stack_a))
 		write(1, "OK\n", 3);
+	free(line);
 }
 
 int	main(int argc, char **argv)
@@ -112,7 +127,9 @@ int	main(int argc, char **argv)
 
 	stack_a = NULL;
 	stack_b = NULL;
-	iferror(argc == 2 && !argv[1][0], EINVAL, "Error\n");
+	if (!argv[1])
+		return (0);
+	iferror(argc == 2 && !argv[1][0], "Error\n");
 	if (argc == 2)
 		argv = split(argv[1], ' ');
 	parsing_args(&stack_a, argv +1);
@@ -123,6 +140,7 @@ int	main(int argc, char **argv)
 		write(1, "OK\n", 3);
 	else
 		lets_check(&stack_a, &stack_b, line);
+	ft_free(&stack_a);
+	ft_free(&stack_b);
 	return (0);
-
 }
