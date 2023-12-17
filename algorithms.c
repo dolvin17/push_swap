fb5b84549c9cdbdb49cb3dc5439e91d2718484ba
ghuertas <ghuertas@student.42madrid.com>
2023-12-17 18:01:40 +0100

    norminette, creating another fn < 25 lines

--- algorithms.c
+++ algorithms.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   algorithms.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
+/*   By: ghuertas <ghuertas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 23:46:52 by ghuertas          #+#    #+#             */
-/*   Updated: 2023/12/11 18:17:24 by dolvin17         ###   ########.fr       */
+/*   Updated: 2023/12/17 17:36:16 by ghuertas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,17 @@ void	order_three(t_stack **stack_a)
 		ft_rra("rra\n", stack_a);
 	if ((*stack_a)->data > (*stack_a)->next->data)
 		ft_sa("sa\n", stack_a);
+}
+
+void	top_min(t_stack **stack_a)
+{
+	while ((*stack_a)->data != min_value(*stack_a)->data)
+	{
+		if (min_value(*stack_a)->top_half)
+			ft_ra("ra\n", stack_a);
+		else
+			ft_rra("rra\n", stack_a);
+	}
 }
 
 void	order_biggest(t_stack **stack_a, t_stack **stack_b)
@@ -46,11 +57,5 @@ void	order_biggest(t_stack **stack_a, t_stack **stack_b)
 		push_back_stack_a(stack_a, stack_b);
 	}
 	set_and_check_pos(*stack_a);
-	while ((*stack_a)->data != min_value(*stack_a)->data)
-	{
-		if (min_value(*stack_a)->top_half)
-			ft_ra("ra\n", stack_a);
-		else
-			ft_rra("rra\n", stack_a);
-	}
+	top_min(stack_a);
 }
